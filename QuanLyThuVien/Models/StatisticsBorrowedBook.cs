874644525d741f619df48ababafd0ef32a5e0751
trunghn2003 <134114519+trunghn2003@users.noreply.github.com>
2024-05-23 10:21:14 +0700

    thong ke

---
+++ QuanLyThuVien/Models/StatisticsBorrowedBook.cs
@@ -0,0 +1,11 @@
+﻿namespace QuanLyThuVien.Models
+{
+    public class StatisticsBorrowedBook
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public int BorrowedCount { get; set; }
+        public string AuthorName { get; set; }
+        public string GenreName { get; set; }
+    }
+}
---
+++ QuanLyThuVien/Repositories/StatisticsBorrowedBook/StatisticsBorrowedBookRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyThuVien.Data;
+using QuanLyThuVien.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Repositories
+{
+    public class StatisticsBorrowedBookRepository : GenericRepository<StatisticsBorrowedBook>
+    {
+        private readonly QuanLyThuVienContext _context;
+
+        public StatisticsBorrowedBookRepository(QuanLyThuVienContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<StatisticsBorrowedBook>> GetAllAsync()
+        {
+            // Retrieve all borrowed books with related Book, Author, and Genre data
+            var borrowedBooks = await _context.BorrowedBook
+                .Include(b => b.Book)
+                .ThenInclude(book => book.Author)
+                .Include(b => b.Book)
+                .ThenInclude(book => book.Genre)
+                .ToListAsync();
+
+            // Group by BookID, BookName, AuthorName, and GenreName and project into StatisticsBorrowedBook
+            var statistics = borrowedBooks
+                .GroupBy(b => new { b.Book.BookID, b.Book.Title, 
+                    AuthorName = b.Book.Author.AuthorName, GenreName = b.Book.Genre.GenreName })
+                .Select(g => new StatisticsBorrowedBook
+                {
+                    BookId = g.Key.BookID,
+                    BookName = g.Key.Title,
+                    BorrowedCount = g.Count(),
+                    AuthorName = g.Key.AuthorName,
+                    GenreName = g.Key.GenreName
+                }).ToList();
+
+            return statistics;
+        }
+    }
+}
